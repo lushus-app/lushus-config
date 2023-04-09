b3b0711b9f7d51e891a07bb877a3de9b1dd12030
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-04-09 11:42:18 -0400

    Update server.rs

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use std::fmt::{Display, Formatter};
 
 #[derive(Clone, Deserialize)]
 pub struct Server {
@@ -13,5 +14,42 @@ impl Server {
 
     pub fn port(&self) -> Option<u16> {
         self.port
+    }
+}
+
+impl Display for Server {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let host = self.host.clone();
+        let result = if let Some(port) = &self.port {
+            format!("{host}:{port}")
+        } else {
+            host
+        };
+        write!(f, "{}", result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::server::Server;
+
+    #[test]
+    fn can_convert_to_string_with_port() {
+        let server = Server {
+            host: "http://localhost".to_string(),
+            port: Some(3000),
+        };
+        let string: String = server.to_string();
+        assert_eq!("http://localhost:3000", string);
+    }
+
+    #[test]
+    fn can_convert_to_string_without_port() {
+        let server = Server {
+            host: "http://localhost".to_string(),
+            port: None,
+        };
+        let string: String = server.to_string();
+        assert_eq!("http://localhost", string);
     }
 }
