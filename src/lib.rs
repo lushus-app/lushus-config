0f219e1fa0b68e5d9309586c8dd91c321d920269
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-04-15 14:04:50 -0400

    Cors config

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod config;
+mod cors;
 mod database;
 mod get_config;
 mod load;
@@ -6,6 +7,7 @@ mod server;
 mod session;
 
 pub use config::Config;
+use cors::Cors;
 use database::Database;
 pub use get_config::get_config;
 pub use load::load;
